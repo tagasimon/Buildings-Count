79479535d300dc59617ea4d209a4d510fb1701bc
Simon Sayz <smkabz@gmail.com>
2020-10-11 23:39:27 +0300

    git commit -m "some code clean up and default images on side bar"

--- count_buildings/app.R
+++ count_buildings/app.R
@@ -4,75 +4,105 @@ library(glue)
 library(shinyjs)
 library(shinycssloaders)
 library(magrittr)
-# library("Bioconductor")
 
 # Define UI for data upload app ----
-ui <- fluidPage(# App title ----
-                titlePanel(Sys.Date()),
-                
-                # Sidebar layout with input and output definitions ----
-                sidebarLayout(
-                    # Sidebar panel for inputs ----
-                    sidebarPanel(
-                        # Input: Select a file ----
-                        fileInput(
-                            "file1",
-                            "Upload Google Map Screen Shot",
-                            multiple = FALSE,
-                            accept = c(".PNG", ".png", "JPG", "JPEG"),
-                            placeholder = "Google Maps Screen Shot",
-                            buttonLabel = "Browse..."
-                        ),
-                        
-                        # Horizontal line ----
-                        tags$hr(),
-                        sliderInput(
-                            "ImageofBuildingThreshol",
-                            label = "Adjust Threshold for better Results",
-                            min = 0.6,
-                            max = 0.8,
-                            step = 0.01,
-                            value = 0.61
-                        ),
-                        tags$hr(),
-                        actionButton(
-                            "calc",
-                            "Calculate",
-                            icon = icon("calculator"),
-                            class = "btn btn-success"
-                        )
-                        
-                    ),
-                    
-                    # Main panel for displaying outputs ----
-                    mainPanel(
-                        titlePanel("Buildings in the Area"),
-                        textOutput("buildings_count"),
-                        tags$hr(),
-                        displayOutput("img") %>%
-                            withSpinner()
-                        
-                    )
-                    
-                ))
+ui <- bootstrapPage(
+    # App title ----
+    useShinyjs(),
+    titlePanel(Sys.Date()),
+    
+    # Sidebar layout with input and output definitions ----
+    sidebarLayout(
+        # Sidebar panel for inputs ----
+        sidebarPanel(
+            # Input: Select a file ----
+            fileInput(
+                "file1",
+                "Step 1: Upload Google Map Screen Shot",
+                multiple = FALSE,
+                accept = c(".PNG", ".png", ".JPG", ".JPEG", "jpg", ".jpeg"),
+                placeholder = "Google Maps Screen Shot",
+                buttonLabel = "Browse..."
+            ),
+            
+            # Horizontal line ----
+            tags$hr(),
+            sliderInput(
+                "ImageofBuildingThreshol",
+                label = "Adjust Threshold for better Results",
+                min = 0.4,
+                max = 0.8,
+                step = 0.01,
+                value = 0.61
+            ),
+            tags$hr(),
+            actionButton(
+                "calc",
+                "Calculate",
+                icon = icon("calculator"),
+                class = "btn btn-success",
+                width = "100%"
+            ),
+            tags$hr(),
+            span(uiOutput("option1"),style="color:red"),
+            displayOutput("imgg", width = "100%", height = "500px") %>%
+                withSpinner()
+            
+        ),
+        
+        # Main panel for displaying outputs ----
+        mainPanel(
+            titlePanel("Buildings in the Area from Google Maps Screen Shot"),
+            tags$hr(),
+            # textOutput("buildings_count"),
+            uiOutput("count_l"),
+            tags$hr(),
+            span(uiOutput("processed"),style="color:red"),
+            displayOutput("img_thres") %>%
+                withSpinner(color = "purple")
+            
+        ),
+        
+        
+    ),
+    tags$head(tags$style("#count_l{color: red;
+                                 font-size: 20px;
+                                 font-style: italic;
+                                 }"
+    )
+    )
+)
 
 # Define server logic to read selected file ----
 server <- function(input, output) {
-    output$img <- renderDisplay({
+    # shinyjs::hideElement("calc")
+    # shinyjs::hideElement("ImageofBuildingThreshol")
+    #
+    ## This code removes the loading icon when the app starts
+    start_loads <- function() {
         req(input$file1)
         tryCatch({
+            shinyjs::showElement("calc")
+            shinyjs::showElement("ImageofBuildingThreshol")
             city_image = readImage(input$file1$datapath)
             display(city_image)
-            
         },
         error = function(e) {
-            # return a safeError if a parsing error occurs
             stop(safeError(e))
         })
+        
+    }
+    output$imgg <- renderDisplay({
+        befr <- readImage('')
+        display(iii)
     })
     
+    output$img_thres <- renderDisplay({
+        befr <- readImage('')
+        display(iii)
+    })
     
-    observeEvent(input$calc, {
+    calc_buildings <- function() {
         req(input$file1)
         tryCatch({
             city_image = readImage(input$file1$datapath)
@@ -81,24 +111,38 @@ server <- function(input, output) {
                 city_image > ImageofBuildingThreshol
             building_count <- bwlabel(city_image_threshold)
             b_count_max <- max(building_count)
-            # b_count_min <- min(building_count)
-            # display(city_image)
-            output$img <- renderDisplay({
+            output$img_thres <- renderDisplay({
                 display(city_image_threshold)
             })
-            output$buildings_count <- renderText({
-                glue::glue("Approximatly ", {
+            output$count_l <- renderText({
+                # shinyjs::disable("calc")
+                glue::glue("Approximatly ~", {
                     b_count_max
                 })
                 # max(b_count_min)
+                
             })
+            
+            output$option1 <- renderText("Uploaded Image")
+            output$processed <- renderText("Processed Image")
+            
             
         },
         error = function(e) {
             # return a safeError if a parsing error occurs
             stop(safeError(e))
         })
+        
+    }
+    
+    observeEvent(input$calc, {
+        calc_buildings()
     })
+    
+    observeEvent(input$ImageofBuildingThreshol, {
+        calc_buildings()
+    })
+    
     
 }
 
